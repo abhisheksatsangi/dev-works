8ca4c443fe90b0759fff51d9c6cbc2eb16c77ddf
abhisheksatsangi <abhisanj@gmail.com>
2018-08-07 15:24:23 +0100

    BNP Test

--- BasketCostCalculator.java
+++ BasketCostCalculator.java
@@ -3,6 +3,6 @@ package com.rbc.testexercise;
 public class BasketCostCalculator {
 	public static void main(String[] args) {
 		
-	  System.out.println(BasketCostDecorator.calculate(new Basket(),Calculable::withApple, Calculable::withOrange,Calculable::withWaterMelon));
+	  System.out.println(BasketCostDecorator.calculate(new Basket(),Calculable::withApple (calculable,5), Calculable::withOrange (calculable,4),Calculable::withWaterMelon(calculable,3));
 	}
 }
