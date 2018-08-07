dbd040e8f8cb29e1d69902861ffe3244c9d99496
abhisheksatsangi <abhisanj@gmail.com>
2018-08-07 12:57:39 +0100

    Update BasketCostCalculator.java
    Modifications

--- BasketCostCalculator.java
+++ BasketCostCalculator.java
@@ -3,6 +3,6 @@ package com.rbc.testexercise;
 public class BasketCostCalculator {
 	public static void main(String[] args) {
 		
-	  System.out.println(BasketCostDecorator.calculate(new Basket(),Calculable::withApple, Calculable::withBanana,Calculable::withOrange,Calculable::withPeach,Calculable::withLemon));
+	  System.out.println(BasketCostDecorator.calculate(new Basket(),Calculable::withApple, Calculable::withOrange,Calculable::withWaterMelon));
 	}
 }
