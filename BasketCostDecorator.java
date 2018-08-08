869ce86666de10086bb4913f30183d7f16850040
abhisheksatsangi <abhisanj@gmail.com>
2018-08-08 12:09:08 +0100

    BNP Test

--- BasketCostDecorator.java
+++ BasketCostDecorator.java
@@ -1,40 +1,56 @@
 package com.bnp.testexercise;
-
 import java.math.BigDecimal;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
 /**
+
  * Decorates a basket by applying lambda constructs reduce and andThen on a stream of 
+
  * instances which can have their costs calculated. In this case, it is the basket of fruits 
+
  * which are deemed as cost-calculable, but in theory it could be anything that could be cost
+
  * calculation worthy.
+
  * @param  calculations - a var-arg of the calculable objects.
+
  * @return the sum cost, expressed as a BigDecimal, of the calculable objects passed in as a parameter.    
+
  * 
+
  */
+
+
 
 public class BasketCostDecorator {
 	
-	private final Function<Calculable, Calculable> totalCost;
+		private final Function<Calculable, Calculable> totalCost;
 
-	@SafeVarargs
-	private BasketCostDecorator(Function<Calculable, Calculable>... calculations) {
-	        this.totalCost = Stream.of(calculations)
-	                .reduce(Function.identity(), Function::andThen);
+		@SafeVarargs
 
-	    }
+		private BasketCostDecorator(Function<Calculable, Calculable>... calculations) {
 
-    @SafeVarargs
-	public static BigDecimal calculate(Calculable calculable, Function<Calculable, Calculable>... calculations) {
-	        return new BasketCostDecorator(calculations).calculate(calculable);
-	    }
-
-	private BigDecimal calculate(Calculable calculable) {
-	    return this.totalCost.apply(calculable).calculateCost();
-	}
+        this.totalCost = Stream.of(calculations).reduce(Function.identity(),Function::andThen);
 
 }
 
+
+
+	    public static BigDecimal calculate(Calculable calculable, Function<Calculable, Calculable>... calculations) {
+
+		        return new BasketCostDecorator(calculations).calculate(calculable);
+
+	    }
+
+
+
+		private BigDecimal calculate(Calculable calculable) {
+
+		    return this.totalCost.apply(calculable).calculateCost();
+
+		}
+
+}
 
 
