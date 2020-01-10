c418b343195570bbc8014c5578f092bfbd7d0459
abhisheksatsangi <abhisanj@gmail.com>
2020-01-10 11:27:43 +0000

    Delete AccountService.java

--- AccountService.java
+++
@@ -1,9 +0,0 @@
-package com.ModularTest.service;
-import java.math.BigDecimal;
-
-public interface AccountService {
-	    String checkBalance(int accountId);
-	    boolean withdrawAmount(int accountId, BigDecimal amount);
-}
-
-
